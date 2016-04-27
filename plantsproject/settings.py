0c28ee467da94c6b454b202422163f6f4725e19f
maminnas <moin.aminnaseri@gmail.com>
2016-04-27 12:35:37 -0700

    modified:   plantsproject/settings.py 	modified:   plantsproject/wsgi.py 	modified:   requirements.txt

--- plantsproject/settings.py
+++ plantsproject/settings.py
@@ -109,9 +109,20 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 #django.contrib.auth.LOGIN_URL = '/frontend/login'
 #django.contrib.auth.LOGIN_REDIRECT_URL = '/frontend/home'
 LOGIN_REDIRECT_URL = '/frontend/'
 LOGIN_URL="/frontend/"
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- plantsproject/wsgi.py
+++ plantsproject/wsgi.py
@@ -10,8 +10,12 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+from whitenoise.django import DjangoWhiteNoise
 
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+
+# application = get_wsgi_application()
