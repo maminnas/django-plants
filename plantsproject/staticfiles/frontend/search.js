b17bb4462b75f57881da00ea2fd1f9d7a160add2
maminnas <moin.aminnaseri@gmail.com>
2016-05-02 04:37:19 -0700

    staticfiles removed

--- plantsproject/staticfiles/frontend/search.js
+++
@@ -1,7 +0,0 @@
-
-$("#search-magnifying-glass").on('click', function(){
-	searchValue = $("#searchbar").val();
-	alert("Searching for " + searchValue)
-
-	$(this).closest(".modal-content").find("form").submit();
-});
