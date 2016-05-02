b6b0c467f63769344a4490612f2551600a860aec
maminnas <moin.aminnaseri@gmail.com>
2016-05-02 05:25:59 -0700

    from .sertting import

--- plantsproject/urls.py
+++ plantsproject/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import include, url, patterns
 from django.contrib import admin
 from login.views import *
 
-import settings
+from .settings import *
 
 # urlpatterns = [
 #     url(r'^admin/', include(admin.site.urls)),
